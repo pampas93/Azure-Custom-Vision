0d3d9bedd12855f140d0c1c995e6bc4d7177da6d
pampas93 <abhijit.93@hotmail.com>
2020-02-29 21:39:28 -0700

    Scene setup with room planes and Spatial Awareness

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoSingleton<RoomManager>
+{
+    [SerializeField]
+    private GameObject roomPlanes;
+
+    void Start()
+    {
+#if !UNITY_EDITOR
+        roomPlanes.SetActive(false);
+#endif
+    }
+}
